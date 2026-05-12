ea852a7fd34154393eb707195a790180e2388fba
Rogério <rogerio7476hora@gmail.com>
2026-05-12 20:37:16 -0300

    Fix: usando regex pura para compatibilidade de rotas

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,8 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('(.*)', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+app.all(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 
